273736700e785f798e3181c2eec85070132bf495
Mason Hawver <mason.hawver@gmail.com>
2021-11-18 14:19:43 -0600

    Update main.c

--- main.c
+++ main.c
@@ -5,8 +5,10 @@
 
 #define POS 0
 #define VEL 1
+#define TAG 2
 
 typedef struct { double x; double y; } Pos, Vel;
+typedef enum { PLAYER, ASTEROID, BULLET } Tag;
 
 void kinematics(ecs * world, float dt, void * data) {
 
@@ -37,14 +39,15 @@ int main(int argc, char const *argv[]) {
   //ADD Componets                ecs world, size of componet, identifyier <- you get to define it
   Componet pos = addComponet_ECS(&world, sizeof(Pos), POS);
   Componet vel = addComponet_ECS(&world, sizeof(Vel), VEL);
+  Componet tag = addComponet_ECS(&world, sizeof(Tag), TAG);
 
   //Add Entities
-  Entity test = addEntity_ECS(&world, 2, pos, vel);
-  Entity cat = addEntity_ECS(&world, 2, pos, vel);
-  Entity bug = addEntity_ECS(&world, 2, pos, vel);
+  Entity player = addEntity_ECS(&world, 3, pos, vel, tag);
+  Entity aster1 = addEntity_ECS(&world, 3, pos, vel, tag);
+  Entity test = addEntity_ECS(&world, 1, pos);
 
   //Modify Componet
-  Pos * p = (Pos*)getEntityComponet_ECS(&world, test, pos);
+  Pos * p = (Pos*)getEntityComponet_ECS(&world, player, pos);
   p->x = 10;
 
   //Create Pipeline
